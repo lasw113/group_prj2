90802caaf4166e8707e101aca022340301e8cb6c
owner <owner@owner-PC>
2017-12-27 18:17:15 +0900

    zzzzzz

--- group_project2_git/src/kr/co/sist/client/run/RunClient.java
+++ group_project2_git/src/kr/co/sist/client/run/RunClient.java
@@ -3,8 +3,8 @@ package kr.co.sist.client.run;
 import kr.co.sist.client.frm.LoginFrm;
 
 public class RunClient {
-
+ 
 	public static void main(String[] args) {
-		new LoginFrm();
+		new LoginFrm();  
 	}// main
 }// class
